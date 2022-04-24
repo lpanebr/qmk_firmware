9c168cc816991abcead48290c2d713ce203c6623
Luciano Panepucci <lpanebr@gmail.com>
2022-04-24 12:13:48 -0300

    base and num layers done!

--- keyboards/draculad/keymaps/lpanebr/keymap.c
+++ keyboards/draculad/keymaps/lpanebr/keymap.c
@@ -30,31 +30,38 @@ enum layer_number {
 
 // Tap Dance declarations
 enum {
-    TD_ESC_CAPS,
+    TD_Q_ESC,
     TD_C_CEDIL,
+    TD_QTS,
+    TD_C_P,
+    TD_2_C,
+    TD_3_P,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
     [TD_C_CEDIL] = ACTION_TAP_DANCE_DOUBLE(KC_C, BR_CCED),
+    [TD_QTS] = ACTION_TAP_DANCE_DOUBLE(BR_QUOT, BR_DQUO),
+    [TD_C_P] = ACTION_TAP_DANCE_DOUBLE(BR_COMM, BR_DOT),
+    [TD_2_C] = ACTION_TAP_DANCE_DOUBLE(KC_2, BR_COMM),
+    [TD_3_P] = ACTION_TAP_DANCE_DOUBLE(KC_3, BR_DOT),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] =  LAYOUT(
-        KC_Q,                 KC_W,             KC_E,            KC_R,            KC_T,                                                 KC_Y,             KC_U,            KC_I,            KC_O,            KC_P,
+        TD(TD_Q_ESC),         KC_W,             KC_E,            KC_R,            KC_T,                                                 KC_Y,             KC_U,            KC_I,            KC_O,            KC_P,
         LGUI_T(KC_A),         LALT_T(KC_S),     LCTL_T(KC_D),    LSFT_T(KC_F),    KC_G,                                                 KC_H,             RSFT_T(KC_J),    LCTL_T(KC_K),    RALT_T(KC_L),    RGUI_T(BR_TILD),
-        LSFT_T(KC_Z),         KC_X,             TD(TD_C_CEDIL),  KC_V,            KC_B,                                                 KC_N,             KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,
+        LSFT_T(KC_Z),         LT(0,KC_X),       TD(TD_C_CEDIL),  LT(0,KC_V),      KC_B,                                                 KC_N,             KC_M,            KC_COMM,         KC_DOT,          LT(0,BR_SLSH),
                                                                                   KC_MUTE,                                              TG(_ADJ),
-                                                                 KC_BSPC,         LT(_NAV,KC_BSPC), LT(_MUS,KC_SPC),    LT(_NUM,KC_DEL), LT(_FUNC,KC_ENT), KC_CAPS
+                                                                 KC_BSPC,         LT(_NAV,KC_SPC), LT(_MUS,KC_TAB),             KC_ENT, LT(_NUM,KC_SPC), LT(_FUNC,KC_DEL)
     ),
     [_NUM] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_GRV,    KC_7,    KC_8,    KC_9,    KC_MINS,
-        KC_TAB,  XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX,                      KC_QUOT, KC_4,    KC_5,    KC_6, KC_PLUS,
-        KC_LSFT, XXXXXXX, XXXXXXX, KC_MUTE, RESET,                        KC_0, KC_1,    KC_2,    KC_3, KC_COMM,
-                                            XXXXXXX,                      KC_NO,
-                                   XXXXXXX, KC_LALT, XXXXXXX,    _______, KC_ENT,  KC_NO
+        TD(TD_QTS),    BR_LCBR, BR_LBRC, BR_LPRN, BR_EXLM,                                                BR_GRV,    KC_7,    KC_8,    KC_9,    KC_MINS,
+        LGUI_T(BR_SLSH), LALT_T(BR_ASTR), LCTL_T(BR_MINS), LSFT_T(BR_PLUS), BR_EQL,                       BR_ACUT, RSFT_T(KC_4),    RCTL_T(KC_5),    RALT_T(KC_6), RGUI_T(KC_PLUS),
+        BR_BSLS, BR_RCBR, BR_RBRC, BR_RPRN, BR_AMPR,                                                      KC_0, KC_1,    TD(TD_2_C),    TD(TD_3_P), TD(TD_C_P),
+                                            XXXXXXX,                                                      KC_NO,
+                                   XXXXXXX, KC_LALT, XXXXXXX,                                    _______, KC_ENT,  KC_NO
     ),
     [_FUNC] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,                       KC_F12,    KC_F7,    KC_F8,    KC_F9,    KC_INS, 
@@ -64,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_LALT, XXXXXXX, XXXXXXX,    XXXXXXX, _______, KC_NO
     ),
     [_NAV] = LAYOUT(
-        KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_LALT, KC_BTN3, KC_BTN2, KC_BTN1, XXXXXXX,                      RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, 
-        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, 
+        KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      BR_CIRC, BR_QUOT, BR_DQUO, BR_DLR, XXXXXXX,
+        KC_LALT, KC_BTN3, KC_BTN2, KC_BTN1, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,
+        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO,
                                             XXXXXXX,                      XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
     ),
@@ -252,3 +259,40 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return true;
 }
 #endif
+
+//LCTL_T(BR_MINS), LSFT_T(BR_PLUS) BR_SLSH
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LALT_T(BR_ASTR):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(BR_ASTR); // Send KC_DQUO on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+        case LCTL_T(BR_MINS):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(BR_MINS); // Send KC_DQUO on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+        case LSFT_T(BR_PLUS):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(BR_PLUS); // Send KC_DQUO on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+        case LT(0,KC_V):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(C(KC_V)); // Intercept hold function to send Ctrl-V
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+        case LT(0,BR_SLSH):
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(BR_SCLN); // Intercept hold function to send Ctrl-V
+                return false;
+            }
+            return true;             // Return true for normal processing of tap keycode
+    }
+    return true;
+}
