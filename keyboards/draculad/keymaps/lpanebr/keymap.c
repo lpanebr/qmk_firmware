1e308291771dc917a5db7d017755ca63baace19e
Luciano Panepucci <lpanebr@gmail.com>
2022-05-02 11:48:34 -0300

    add F13 to F24 and copy and paste

--- keyboards/draculad/keymaps/lpanebr/keymap.c
+++ keyboards/draculad/keymaps/lpanebr/keymap.c
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                           KC_BSPC, _______, XXXXXXX,                               KC_ENT, KC_NO,  KC_DEL
     ),
     [_FUNC] = LAYOUT(
-        _______, _______,   _______,  _______,  _______,                                                           KC_F12, KC_F7, KC_F8, KC_F9, KC_INS,
-        _______, _______,   _______,  _______,  _______,                                                           KC_F11, KC_F4, KC_F5, KC_F6, XXXXXXX,
-        _______, _______,   _______,  _______,  _______,                                                           KC_F10, KC_F1, KC_F2, KC_F3, XXXXXXX,
-                                                XXXXXXX,                                                           KC_NO,
-                              KC_LALT, XXXXXXX, XXXXXXX,                                                           KC_ENT, _______, _______
+        KC_F24, KC_F23, KC_F22, KC_F21, KC_PSTE,                                                                   KC_F12, KC_F7, KC_F8, KC_F9, KC_INS,
+        KC_F20, KC_F19, KC_F18, KC_F17, KC_COPY,                                                                   KC_F11, KC_F4, KC_F5, KC_F6, XXXXXXX,
+        KC_F16, KC_F15, KC_F14, KC_F13, XXXXXXX,                                                                   KC_F10, KC_F1, KC_F2, KC_F3, XXXXXXX,
+                                        XXXXXXX,                                                                   KC_NO,
+                      KC_LALT, XXXXXXX, XXXXXXX,                                                                   KC_ENT, _______, _______
     ),
     [_NAV] = LAYOUT(
         _______, _______, _______, _______, _______,                                                               BR_CIRC, BR_QUOT, BR_DQUO, BR_DLR, XXXXXXX,
