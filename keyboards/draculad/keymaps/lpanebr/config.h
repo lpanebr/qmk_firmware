acb187445721a15da64414c03dbe646cd8f29cb6
Luciano Panepucci <lpanebr@gmail.com>
2022-12-03 12:03:34 -0300

    Fixes my problem of getting ß and æ when I type ls and la quickly.

--- keyboards/draculad/keymaps/lpanebr/config.h
+++ keyboards/draculad/keymaps/lpanebr/config.h
@@ -3,3 +3,7 @@
 #define TAPPING_TERM 175
 #define MK_COMBINED
 // #define RGBLIGHT_LAYERS
+
+// This only affects Mod Tap keys like I use for homerow mods.
+// Fixes my problem of getting ß and æ when I type ls and la quickly.
+#define IGNORE_MOD_TAP_INTERRUPT
